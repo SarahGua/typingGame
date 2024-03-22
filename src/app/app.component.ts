37ce88b4351d35221a8c2bd142cd4a2042ec3adf
SarahGuarneri <sarahguarneri97@icloud.com>
2024-03-22 17:07:36 +0100

    app complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,35 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {lorem} from 'faker';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'typingGameAngular';
+
+  enteredText = ''
+  randomText = lorem.sentence()
+
+  compare(randomLetter: string, enteredLetter: string){
+    if(!enteredLetter){
+      return 'pending'
+    }
+
+    return randomLetter === enteredLetter ? 'correct' : 'incorrect'
+
+  }
+
+  onChangeInputText(event: Event){
+    const target = event.target as HTMLInputElement
+    const text = target.value
+    console.log(text)
+    this.enteredText = text
+    console.log(this.enteredText)
+  }
+
 }
